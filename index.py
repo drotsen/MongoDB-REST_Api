63e919bc3991378748538d99629237fad1dd5cc5
drotsen <marko.thoren@gmail.com>
2023-03-14 19:33:28 +0100

    third change

--- index.py
+++ index.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
+from routes.user import user
 import uvicorn
 
 app = FastAPI()
+app.include_router(user)
 
-@app.get("/")
-async def root():
-    return {"Hej"}
+# @app.get("/")
+# async def root():
+#     return {"Hej"}
 
--- routes/user.py
+++ routes/user.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
-
 from models.user import User
-
+from schemas.user import userEntity,usersEntity
 from config.db import conn
-
 
 user= APIRouter()
 
 @user.get('/')
 async def find_all_users():
-    return conn.local.user.find()
+    print(conn.local.user.find())
+    print(usersEntity( conn.local.user.find()))
+    return usersEntity( conn.local.user.find())
